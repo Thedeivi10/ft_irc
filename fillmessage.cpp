d076f4c3bf40a12f83b9a7932d06839af1726e74
David Gomez-Landero Lopez <davigome@c3r5s2.42malaga.com>
2025-06-21 15:42:45 +0200

    editing fillmessage with all the rpl, cotinuing

--- fillmessage.cpp
+++ fillmessage.cpp
@@ -23,12 +23,105 @@ void Server::sendfillmessage(int mesage_type, std::string channelName, int fd)
 		case (CMD_JOIN):
 			joinMsg = ":" + client->getNickName() +"!" + client->getUserName() + "@localhost JOIN #" + channel->getChannelName() +  "\r\n";
 			break;
+		case (RPL_WELCOME):
+			joinMsg = ":" + this->name + " 001 " + client->getNickName() + " :Welcome to IRC" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 002 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		/* case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 003 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 004 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 005 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 324 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 329 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 331 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break; */
+		/* case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 332 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 333 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 341 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 352 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break; */
 		case (RPL_NAMREPLY):
 			joinMsg = ":" + this->name + " 353 " + client->getNickName() + " = #" + channel->getChannelName() + " :@" + client->getNickName() + "\r\n";
 			break;
 		case (RPL_ENDOFNAMES):
 			joinMsg = ":" + this->name + " 366 " + client->getNickName() + " #" + channel->getChannelName() + " :End of NAMES list" + "\r\n";
 			break;
+		/* case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 401 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 403 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 404 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 412 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 432 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 433 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 441 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 442 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 443 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 461 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 462 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 467 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 471 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 472 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 473 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 475 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 464 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 482 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break;
+		case (RPL_YOURHOST):
+			joinMsg = ":" + this->name + " 696 " + client->getNickName() + " :Your host is localhost.irc" + "\r\n";
+			break; */
 		default:
 			break;
 	}
